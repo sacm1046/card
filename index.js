5bbccd48f41e554526c96f132b0e4adc3d7d82ac
Sebastian Contreras <sebastian@clever.cl>
2022-10-09 19:37:15 -0300

    addevent listeners

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const buttonPikachu = document.getElementById("button__pikachu")
-console.log(buttonPikachu)
-
 const displayNumber = document.getElementById("display__number")
-let display = 0
+const buttonAdd = document.getElementById("button__pikachu")
 
-const onClick = () => {
-    display = display + 1
-    displayNumber.innerText = display
+const onClick = (displayConst) => {
+    let value = Number(displayConst.innerText)
+    value++
+    displayConst.innerText = value
 }
+
+buttonAdd.addEventListener('click', () => onClick(displayNumber))
+
+////////////////////////////////////////////////
+
+const displayNumber2 = document.getElementById("display__number2")
+const buttonAdd2 = document.getElementById("button__pikachu2")
+
+buttonAdd2.addEventListener('click', () => onClick(displayNumber2))
+
 
